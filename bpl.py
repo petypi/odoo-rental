0aec64edfc36676321d9db80ac8b74a9e5f0fd95
priyankahdp <priyankahdp@gmail.com>
2013-03-29 16:46:06 +0530

    Update bpl.py

--- bpl.py
+++ bpl.py
@@ -36,7 +36,7 @@ class bpl_officer_registration(osv.osv):
 bpl_officer_registration()
 
 class bpl_worker_registration(osv.osv):
-        
+
     def on_change_indic_cost(self, cr, uid, ids, cost_ids, context=None):
         totalsum = 0.0
         for element in cost_ids:
@@ -45,9 +45,9 @@ class bpl_worker_registration(osv.osv):
             'value': {
                 'sum_cost': totalsum,
             }
-        }        
-        
-    
+        }
+
+
     def on_change_estate(self, cr, uid, ids, estate_id, context=None):
         if estate_id:
             estate_object = self.pool.get('bpl.estate.n.registration')
@@ -62,7 +62,7 @@ class bpl_worker_registration(osv.osv):
             employer_no = employer_no_object.browse(cr, uid, employers_id, context=context)
 #            employer_browse = employer_no_object.browse(cr, uid, estate_id, context=context)
             return {'value': {'employer_no': employer_no[0].epf_no }}
-    
+
 
     def _max_reg_no(self, cr, uid, context=None):
         cr.execute("""
@@ -78,9 +78,6 @@ class bpl_worker_registration(osv.osv):
         result = 'W' + (str(emp_no_int + 1).zfill(4))
         return result
 
-
-
-        
     def create(self, cr, uid, values, context=None):
         values['register_no'] = self.pool.get('ir.sequence').get(cr, uid, 'bpl.worker')
 #        values['employer_no'] = self.pool.get('ir.sequence').get(cr, uid, 'bpl.employer')
@@ -91,13 +88,13 @@ class bpl_worker_registration(osv.osv):
         for record in self.browse(cr, uid, ids, context=context):
             res[record.id] = 'BPL001/' + record.register_no
         return res
-    
+
     def _default_company(self, cr, uid, context=None):
         user = self.pool.get('res.users').browse(cr, uid, uid, context=context)
         if user.company_id:
             return user.company_id.id
-        return self.pool.get('bpl.company.n.registration').search(cr, uid, [])[0]
-    
+        return self.pool.get('bpl.company.n.registration').search(cr, uid, search_condition)[0]
+
     _name = "bpl.worker"
     _description = "Worker registration details"
     _columns = {
@@ -128,11 +125,11 @@ class bpl_worker_registration(osv.osv):
         'default_work': fields.selection([('tea', 'Tea'), ('rubber', 'Rubber'), ('sundry', 'Sundry')], 'Default Work'),
         'union_id': fields.many2one('bpl.union', 'Union', help='Union'),
         'estate_resident': fields.boolean('Estate Resident', help="Estate resident or not"),
-        
+
         'address_line_1': fields.char('Address Line 1', size=256, help='Address Line 1'),
         'address_line_2': fields.char('Address Line 2', size=256, help='Address Line 2'),
         'address_line_3': fields.char('Address Line 3', size=256, help='Address Line 3'),
-        
+
         'occupation_code': fields.char('Occupation Code', size=32, help='Occupation Code'),
         'is_active': fields.boolean('Inactive', help="Is active or not"),
         'current_status': fields.selection([('suspend', 'Suspend'), ('terminate', 'Terminate'), ('dismiss', 'Dismiss')], 'Current Status'),
@@ -184,11 +181,11 @@ class union_registration(osv.osv):
 union_registration()
 
 class company_define(osv.osv):
-    
+
     def onchange_is_company(self, cr, uid, ids, is_company, context=None):
         print('this works my on change')
         return {'value': {'min': is_company}}
-    
+
     _name = "bpl.company.define"
     _description = "Company Definition"
     _columns = {
@@ -213,37 +210,59 @@ class employer_epf(osv.osv):
 employer_epf()
 
 class bpl_work_offer(osv.osv):
-    
-    def onchange_division(self, cr, uid, ids, division_id, context=None):
+
+    def on_change_division(self, cr, uid, ids, division_id):
+        tea_v = {}
+        tea_list_data = []
+        
+        rubber_v = {}
+        rubber_list_data = []
+        
+        sundry_v = {}
+        sundry_list_data = []
+        
         if division_id:
-            division_self = self.pool.get('bpl.division.n.registration')
-            division_browse = division_self.browse(cr, uid, division_id, context=context)
-            result_id = division_browse.id
-            worker_object = self.pool.get('bpl.worker')            
-            search_condition = [
-                              ('bpl_division_id', '=', result_id)
-                              ]
-            worker_list = worker_object.search(cr, uid, search_condition, context=context)
-            return worker_list
+            
+            tea_worker_ids = self.pool.get('bpl.worker').search(cr, uid, [('bpl_division_id', '=', division_id)])
+            for record in self.pool.get('bpl.worker').browse(cr, uid, tea_worker_ids):
+                tea_list_data.append({'worker_id': record.id})
+            tea_v['selected_tea_workers_line_ids'] = tea_list_data
+            
+        if division_id:    
+            rubber_worker_ids = self.pool.get('bpl.worker').search(cr, uid, [('bpl_division_id', '=', division_id)])
+            for record in self.pool.get('bpl.worker').browse(cr, uid, rubber_worker_ids):
+                rubber_list_data.append({'worker_id': record.id})
+            rubber_v['selected_rubber_workers_line_ids'] = rubber_list_data
+            
+        if division_id:
+            sundry_worker_ids = self.pool.get('bpl.worker').search(cr, uid, [('bpl_division_id', '=', division_id)])
+            for record in self.pool.get('bpl.worker').browse(cr, uid, sundry_worker_ids):
+                sundry_list_data.append({'worker_id': record.id})
+            sundry_v['selected_sundry_workers_line_ids'] = sundry_list_data
+            
+            tea_v.update(rubber_v)
+            tea_v.update(sundry_v)
+            return {'value':tea_v}
+            
 
     def on_change_country(self, cr, uid, ids, country_id, context=None):
         currency_id = self._get_euro(cr, uid, context=context)
         if country_id:
             currency_id = self.pool.get('res.country').browse(cr, uid, country_id, context=context).currency_id.id
         return {'value': {'currency_id': currency_id}}
-    
+
     def _get_selection(self, cursor, user_id, context=None):
         return (
                 ('head_office', 'Head Office'),
                 ('estate', 'Estate'))
-    
+
     def _sel_func(self, cr, uid, context=None):
         obj = self.pool.get('bpl.officer')
         ids = obj.search(cr, uid, [])
         res = obj.read(cr, uid, ids, ['name', 'id'], context)
         res = [(r['id'], r['name']) for r in res]
         return res
-    
+
     _name = "bpl.work.offer"
     _description = "BPL Work Offer"
     _columns = {
@@ -265,7 +284,7 @@ class bpl_work_offer(osv.osv):
         'selected_sundry_workers_line_ids': fields.one2many('bpl.selected.sundry.workers.line', 'sundry_line_worker_id', 'Sundry Workers', ondelete="cascade"),
         'is_confirmed': fields.boolean('Is Confirmed', help="confirmed"),
     }
-    
+
     _defaults = {
                  'is_confirmed': False,
                  # 'user_id':_sel_func,
@@ -280,9 +299,12 @@ class selected_tea_workers_line_ids(osv.osv):
     _columns = {
         'tea_line_worker_id':fields.many2one('bpl.work.offer', 'Tea Worker Line', ondelete='cascade', help='Worker Line'),
         'worker_id':fields.many2one('bpl.worker', 'Tea Worker', ondelete='cascade', help='Tea Worker'),
-        'is_selected': fields.boolean('Select', help="Selected or not"),
+        'is_selected': fields.boolean('Select', select=True, help="Selected or not"),
         }
-        
+    _defaults = {
+                 'is_selected': True,
+                 }
+
 selected_tea_workers_line_ids()
 
 class selected_rubber_workers_line_ids(osv.osv):
@@ -291,9 +313,12 @@ class selected_rubber_workers_line_ids(osv.osv):
     _columns = {
         'rubber_line_worker_id':fields.many2one('bpl.work.offer', 'Rubber Worker', ondelete='cascade', help='Worker'),
         'worker_id':fields.many2one('bpl.worker', 'Rubber Worker', ondelete='cascade', help='Rubber Worker'),
-        'is_selected': fields.boolean('Select', help="Selected or not"),
+        'is_selected': fields.boolean('Select', select=True, help="Selected or not"),
         }
-        
+    _defaults = {
+                 'is_selected': True,
+                 }
+
 selected_rubber_workers_line_ids()
 
 class selected_sundry_workers_line_ids(osv.osv):
@@ -302,19 +327,22 @@ class selected_sundry_workers_line_ids(osv.osv):
     _columns = {
         'sundry_line_worker_id':fields.many2one('bpl.work.offer', 'Sundry Worker', ondelete='cascade', help='Sundry Worker'),
         'worker_id':fields.many2one('bpl.worker', 'Sundry Worker', ondelete='cascade', help='Sundry Worker'),
-        'is_selected': fields.boolean('Select', help="Selected or not"),
+        'is_selected': fields.boolean('Select', select=True, help="Selected or not"),
         }
-        
+    _defaults = {
+                 'is_selected': True,
+                 }
+
 selected_sundry_workers_line_ids()
 
 class company_estate_division(osv.osv):
-    
+
     def _get_address_data(self, cr, uid, ids, field_names, arg, context=None):
         return self.pool.get('bpl.company.n.registration').search(cr, uid, [])[0]
 
     def _set_address_data(self, cr, uid, company_id, name, value, arg, context=None):
-        return True    
-    
+        return True
+
     _name = 'bpl.company.estate.division'
     _description = 'bpl company estate division mapping'
     _columns = {
@@ -324,11 +352,11 @@ class company_estate_division(osv.osv):
 
         'company_id': fields.function(_get_address_data, fnct_inv=_set_address_data, type='many2one', relation='bpl.company.n.registration', string="Country New",
         multi='address'),
-        
+
         'state_id': fields.function(_get_address_data, fnct_inv=_set_address_data, type='many2one', domain="[('company_id', '=', bpl_company_id)]",
         relation='bpl.estate.n.registration', string="Estate New"),
                 }
-    
+
 company_estate_division()
 #######################################################################################################
 class company_new_registration(osv.osv):
@@ -336,7 +364,7 @@ class company_new_registration(osv.osv):
     _description = "Company"
     _columns = {
                 'name': fields.char('Company Name', size=128, required=True),
-                'estates': fields.one2many('bpl.estate.n.registration', 'company_id', 'Estate')                                        
+                'estates': fields.one2many('bpl.estate.n.registration', 'company_id', 'Estate')
     }
 # Bill of Material
 company_new_registration()
@@ -375,6 +403,7 @@ class sundry_work_registration(osv.osv):
 sundry_work_registration()
 
 class bpl_work_update(osv.osv):
+
     _name = "bpl.work.update"
     _description = "BPL Work Update"
     _columns = {
@@ -405,7 +434,7 @@ class selected_tea_workers_update_line_ids(osv.osv):
         'tea_total_kgs':fields.integer('Total Kgs'),
         'tea_over_kgs':fields.integer('Over Kgs'),
         }
-        
+
 selected_tea_workers_update_line_ids()
 
 class selected_rubber_workers_update_line_ids(osv.osv):
@@ -421,7 +450,7 @@ class selected_rubber_workers_update_line_ids(osv.osv):
         'rubber_over_kgs':fields.integer('Over Kgs'),
         'rubber_scrap_kgs':fields.integer('Scrap Kgs'),
         }
-        
+
 selected_rubber_workers_update_line_ids()
 
 class selected_sundry_workers_update_line_ids(osv.osv):
@@ -434,5 +463,5 @@ class selected_sundry_workers_update_line_ids(osv.osv):
         'sundry_hrs_worked':fields.integer('Hours worked'),
         'sundry_work_id':fields.many2one('bpl.sundry.work.registration', 'Sundry Work', help='Sundry Work'),
         }
-        
+
 selected_sundry_workers_update_line_ids()
