b49df07ac941a19eb40f8fb13d6a28a2ff895fb4
priyankahdp <priyankahdp@gmail.com>
2013-04-12 13:49:26 +0530

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'ERP System',
+    'author': 'Bellvantage (Pvt) Ltd',
+    'version': '1.0',
+    'depends': ['base', 'hr'],
+    'category' : 'Production',
+    'summary': 'ERP Information System for Plantation Industry',
+    'description': """
+    """,
+    'data': [
+             'bpl_worker_registration_sequence.xml',
+             'bpl_view.xml',
+             'security/bpl_security.xml'],
+    'css':[],
+    'images': [],
+    'demo': [],
+    'installable': True,
+    'application' : True,
+    'certificate' : '',
+    'images': [],
+}
